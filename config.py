b50c183d0604f468d201aff4477b16bb61d5cb35
vikapavsk <120643047+vikapavsk@users.noreply.github.com>
2023-01-29 17:54:56 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '5877029836:AAE_eKN4a_uEeZn-UNa_-z6NdUNNExXGfRI'
+
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'юань': 'CNY',
+    'фунт стерлингов': 'GBP'
+}
